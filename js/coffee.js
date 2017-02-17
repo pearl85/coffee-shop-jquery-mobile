6d4e958621d23e009718c74bb26c690bedc2a4df
PearlSan <pearlsankar85@gmail.com>
2017-02-16 23:39:33 -0600

    added coffee shop

---
+++ js/coffee.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function() {
+    $("#offer").on("tap", function() {
+        $(this).hide();
+        $("#top-grey-bar").addClass("grey-bar").addClass("grey-bar-23px");
+        $("#order-coffee-page-link").attr("href", "#coffee-order-confirm");
+        event.preventDefault();
+    });
+});
